f78c0d97675bfd2572d32977021c37186000a865
Joel <joel.balmer@gmail.com>
2020-01-20 14:09:09 +0000

    signal r working with client js

--- PlanningPoker/Startup.cs
+++ PlanningPoker/Startup.cs
@@ -19,15 +19,17 @@ namespace PlanningPoker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddCors(options => options.AddPolicy("CorsPolicy", builder => {
+            services.AddCors(options => options.AddPolicy("CorsPolicy", builder =>
+            {
                 builder.AllowAnyMethod()
                     .AllowAnyHeader()
                     .AllowCredentials()
-                    .WithOrigins("http://localhost:4200/");
+                    .WithOrigins("http://localhost:4200");
             }));
 
             services.AddSignalR();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
