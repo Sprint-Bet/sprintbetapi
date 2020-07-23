b2d2c54efc7e78c522aef6ec9765573122f4f978
Joel Balmer INTO <Joel.Balmer@intoglobal.com>
2020-07-23 17:56:18 +0100

    Added new roomitems  type

--- SprintBetApi/Dtos/NewRoomDto.cs
+++ SprintBetApi/Dtos/NewRoomDto.cs
@@ -1,10 +1,14 @@
-﻿namespace SprintBet.Dtos
+﻿using SprintBet.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SprintBet.Dtos
 {
     public class NewRoomDto
     {
         /// <summary>
         ///     Key to determine which items the room should use
         /// </summary>
-        public string ItemsType { get; set; }
+        [Required]
+        public RoomType ItemsType { get; set; }
     }
 }
--- SprintBetApi/Services/RoomService.cs
+++ SprintBetApi/Services/RoomService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using SprintBet.Dtos;
+using SprintBet.Enums;
 
 namespace SprintBet.Services
 {
@@ -25,12 +26,21 @@ namespace SprintBet.Services
         }
 
         /// <inheritdoc/>
-        public Room AddRoom(string roomType)
+        public Room AddRoom(RoomType roomType)
         {
             string[] fibonacciItems = { "1", "2", "3", "5", "8", "13", "20", "?" };
+            string[] spikeItems = { "1", "2", "3", "4", "5", "6", "7", "8" };
             string[] tshirtItems = {"XXS", "XS", "S", "M", "L", "XL", "XXL", "?"};
-
-            var items = (roomType == "fibonacci") ? fibonacciItems : tshirtItems;
+            
+            var items = fibonacciItems;
+            if (roomType == RoomType.Spike)
+            {
+                items = spikeItems;
+            }
+            else if (roomType == RoomType.TShirtSizing)
+            {
+                items = tshirtItems;
+            }
 
             var newRoom = new Room
             {
