d965db08ee9b0546585c729390813b9c2c8e9aad
Joel Balmer INTO <Joel.Balmer@intoglobal.com>
2020-09-30 17:48:58 +0100

    added custom claim to token

--- SprintBetApi/Services/AuthService.cs
+++ SprintBetApi/Services/AuthService.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using SprintBetApi.Constants;
+using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Microsoft.IdentityModel.Tokens;
-using System.IdentityModel.Tokens.Jwt;
-using SprintBetApi.Dtos;
 
 namespace SprintBetApi.Services
 {
@@ -11,27 +11,25 @@ namespace SprintBetApi.Services
     public class AuthService : IAuthService
     {
         /// <inheritdoc/>
-        public string GenerateToken(AuthenticateVoterDto authenticateVoterDto)
+        public string GenerateToken(string voterId, string roomId)
         {
 			var mySecret = "placeholder secret";
 			var mySecurityKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(mySecret));
 
-			var myIssuer = "https://sprintbetapi.herokuapp.com/";
-			var myAudience = "https://sprintbet.herokuapp.com/";
-
-			var tokenHandler = new JwtSecurityTokenHandler();
 			var tokenDescriptor = new SecurityTokenDescriptor
 			{
 				Subject = new ClaimsIdentity(new Claim[]
 				{
-					new Claim(ClaimTypes.NameIdentifier, authenticateVoterDto.Id),
+					new Claim(ClaimTypes.NameIdentifier, voterId),
+					new Claim(CustomClaims.RoomId, roomId)
 				}),
 				Expires = DateTime.UtcNow.AddHours(4),
-				Issuer = myIssuer,
-				Audience = myAudience,
+				Issuer = "https://sprintbetapi.herokuapp.com/",
+				Audience = "https://sprintbet.herokuapp.com/",
 				SigningCredentials = new SigningCredentials(mySecurityKey, SecurityAlgorithms.HmacSha256Signature)
 			};
 
+			var tokenHandler = new JwtSecurityTokenHandler();
 			var token = tokenHandler.CreateToken(tokenDescriptor);
 			return tokenHandler.WriteToken(token);
 		}
