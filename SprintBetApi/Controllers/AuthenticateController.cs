4dd1287f702c23db69d16236387484e2c43ad7b7
Joel Balmer INTO <Joel.Balmer@intoglobal.com>
2020-10-14 16:32:23 +0100

    was only for debugging

--- SprintBetApi/Controllers/AuthenticateController.cs
+++ SprintBetApi/Controllers/AuthenticateController.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SprintBetApi.Dtos;
 using SprintBetApi.Services;
-using System.Linq;
-using System.Security.Claims;
 
 namespace SprintBetApi.Controllers
 {
@@ -27,27 +25,6 @@ namespace SprintBetApi.Controllers
 
             var token = _authService.GenerateToken(authenticateVoterDto.Id, authenticateVoterDto.RoomId);
             return Ok(token);
-        }
-
-        [HttpGet("voter/{voterId}")]
-        public IActionResult DecodeToken([FromRoute] string voterId, [FromHeader] string Authorization)
-        {
-            var voterIdFromRoute = voterId;
-            var authHeader = Authorization;
-            if (string.IsNullOrWhiteSpace(voterIdFromRoute) || string.IsNullOrWhiteSpace(authHeader))
-            {
-                return BadRequest("blah");
-            }
-
-            var token = _authService.ReadToken(authHeader);
-            var voterIdFromToken = token.Claims.First(claim => claim.Type == ClaimTypes.NameIdentifier).ToString();
-            if (voterIdFromRoute == voterIdFromToken)
-            {
-                return Unauthorized("doesn't match");
-            }
-
-            return Ok(new { VoterIdFromRoute = voterIdFromRoute, VoterIdFromToken = voterIdFromToken });
-
         }
     }
 }
