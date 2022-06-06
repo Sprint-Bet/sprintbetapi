ab8786ed2c2551307b4bb32dfe36a356473cf558
joel.balmer <Joel.Balmer@intoglobal.com>
2022-06-06 21:58:03 +0100

    added app insights

--- SprintBetApi/Startup.cs
+++ SprintBetApi/Startup.cs
@@ -48,6 +48,8 @@ namespace SprintBet
 
       services.AddControllers().AddNewtonsoftJson();
       services.AddHealthChecks();
+
+      services.AddApplicationInsightsTelemetry(Configuration);
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
